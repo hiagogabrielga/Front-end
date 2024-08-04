860653dda9216c87957d6415e49c49910240cbd5
HiagoGGA <hiagogabriel1132@gmail.com>
2024-08-04 12:09:32 -0400

    ajustes

--- Projeto-linguagens/javaScript/script.js
+++ Projeto-linguagens/javaScript/script.js
@@ -6,3 +6,17 @@ function showMenu(){
         menuMobile.classList.add('open')
     }
 }
+
+
+function adjustIframeHeight() {
+    var iframe = document.getElementById('myIframe');
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+}
+
+// Ajusta a altura quando o iframe terminar de carregar
+document.getElementById('myIframe').onload = function () {
+    adjustIframeHeight();
+};
+
+// Ajusta a altura também quando o conteúdo interno mudar (se aplicável)
+window.addEventListener('resize', adjustIframeHeight);
