c38c87a694c301f7e28d2f060eafca354836d563
HiagoGGA <hiagogabriel1132@gmail.com>
2024-08-31 10:16:09 -0400

    atualizacao

--- Portfolio/javaScript/menuResponsivo.js
+++
@@ -1,8 +0,0 @@
-function showMenu(){
-    let menuMobile = document.querySelector('.navBarHeaderMobile');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
-    } else {
-        menuMobile.classList.add('open')
-    }
-}
--- Portfolio/javaScript/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function () {
-    let header = document.querySelector('#header-bonito')
-    let conteudo = document.querySelector('.conteudo-central')
-    header.classList.toggle('rolagem', window.scrollY > 0)
-    conteudo.classList.toggle('rolagem', window.scrollY > 100)
-})
